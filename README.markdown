daf999046d222b06bcc7da597c5630fec9064e01
Matthias Geisler <matthias@semanticdreamer.com>
2013-02-13 22:59:54 +0100

    basic pouchdb app working (no file uploads yet)

--- README.markdown
+++ README.markdown
@@ -2,6 +2,14 @@
 
 A little sample application for uploading files to a [PouchDB][pouchdb.com].
 
+## Building Blocks
+
+- HTML5
+- [jQuery][jquery.com] (v1.9.1)
+- [Underscore.js][underscorejs.org] (v.1.4.4)
+- [PouchDB][pouchdb.com]
+- [mustache.js][mustache.js]
+
 ## Credits
 
 - Prime free font - unique sans serif techy fonts by [Fontfabric™][fontfabric.com].
@@ -10,3 +18,6 @@ A little sample application for uploading files to a [PouchDB][pouchdb.com].
 [pouchdb.com]: http://pouchdb.com
 [fontfabric.com]: http://fontfabric.com/prime-free-font/
 [barebones]: http://barebones.paulrobertlloyd.com
+[jquery.com]: http://jquery.com
+[mustache.js]: https://github.com/janl/mustache.js
+[underscorejs.org]: http://underscorejs.org/
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,44 @@
-/**
- * Created with IntelliJ IDEA.
- * User: matthias
- * Date: 13.02.13
- * Time: 14:16
- * To change this template use File | Settings | File Templates.
- */
-var pouchdb;
-Pouch('idb://pouchdb-uploads', function(err, db) {
-    pouchdb = db;
+$(function() {
+    var db = 'idb://pouchdb-uploads';
+    var pouchdb;
+
+    function createPouchDB() {
+        Pouch(db, function(err, db) {
+            pouchdb = db;
+            console.log(db);
+            loadUploads();
+        });
+    }
+    createPouchDB();
+
+    function loadUploads(){
+        pouchdb.allDocs({include_docs: true}, function(err, res) {
+            _.each(res.rows, function(val, key, list){
+                var upload = Mustache.to_html(
+                    $('#tpl-upload').html(),
+                    val.doc
+                );
+                $('#uploads-list').append(upload);
+            });
+        });
+    }
+
+    $('#btn-clear-db').click(function(e){
+        Pouch.destroy(db, function(err, info) {
+            console.log(info);
+            $('#uploads-list').empty();
+            createPouchDB();
+        });
+    });
+
+    $('#upload-form').submit(function(e){
+        e.preventDefault();
+        var n = $('#new-upload-name');
+        pouchdb.post({name: n.val()}, function(err, res){
+            console.log(res);
+            n.val('');
+            loadUploads();
+        });
+    });
 });
+
